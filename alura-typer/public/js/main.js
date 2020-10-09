41480d31e09ae1a39429c5386c1112368ee66bfc
Gustavo Santos <gustavo.br.sant@hotmail.com>
2020-10-08 22:40:03 -0300

    Adição das funcionafidade de contar a quantidade de palavras no paragrafo

---
+++ alura-typer/public/js/main.js
@@ -0,0 +1,13 @@
+// O simbolo $ é a mesma coisa que usar a função Jquery, de uma forma simplificada
+// e nesse caso, esta buscando no html o elemento com a classe frase e chamendo o
+// metodo text para trazer apenas o texto dessa tag
+var frase = $(".frase").text();
+
+// Conta a quantidade de palavras separando cada palavra por espaço e contando cada uma.
+var numeroPalavras = frase.split(" ").length;
+
+// Traz um objeto jquery do elemento html com o id tamanho-frase
+var tamanhoFrase = $("#tamanho-frase");
+
+// A função text, quando passado parâmetros, insere elementos dentro da tag
+tamanhoFrase.text(numeroPalavras);
